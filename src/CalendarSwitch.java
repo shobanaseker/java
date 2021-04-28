e771f4b196798a611adb0f8939e3add420bb6829
shobana <shobanaseker@gmail.com>
2021-04-28 21:58:34 +0530

    Calendar using switch

---
+++ src/CalendarSwitch.java
@@ -0,0 +1,72 @@
+public class CalendarSwitch {
+    public static void main(String[] args) {
+        int date, month, year = 2020;
+        for (month = 1; month <= 12; month++) {
+            switch (month) {
+
+
+                case 1:
+                    for (date = 1; date <= 31; date++) {
+                        System.out.println(year + " " + month + " " + date);
+                    }
+                    break;
+                case 2:
+                    for (date = 1; date <= 28; date++) {
+                        System.out.println(year + " " + month + " " + date);
+                    }
+                    break;
+                case 3:
+                    for (date = 1; date <= 31; date++) {
+                        System.out.println(year + " " + month + " " + date);
+                    }
+                    break;
+                case 4:
+                    for (date = 1; date <= 30; date++) {
+                        System.out.println(year + " " + month + " " + date);
+                    }
+                    break;
+                case 5:
+                    for (date = 1; date <= 31; date++) {
+                        System.out.println(year + " " + month + " " + date);
+                    }
+                    break;
+                case 6:
+                    for (date = 1; date <= 30; date++) {
+                        System.out.println(year + " " + month + " " + date);
+                    }
+                    break;
+                case 7:
+                    for (date = 1; date <= 31; date++) {
+                        System.out.println(year + " " + month + " " + date);
+                    }
+                    break;
+                case 8:
+                    for (date = 1; date <= 31; date++) {
+                        System.out.println(year + " " + month + " " + date);
+                    }
+                    break;
+                case 9:
+                    for (date = 1; date <= 30; date++) {
+                        System.out.println(year + " " + month + " " + date);
+                    }
+                    break;
+                case 10:
+                    for (date = 1; date <= 31; date++) {
+                        System.out.println(year + " " + month + " " + date);
+                    }
+                    break;
+                case 11:
+                    for (date = 1; date <= 30; date++) {
+                        System.out.println(year + " " + month + " " + date);
+                    }
+                    break;
+                case 12:
+                    for (date = 1; date <= 31; date++) {
+                        System.out.println(year + " " + month + " " + date);
+                    }
+                    break;
+
+            }
+        }
+    }
+}
